c4535253b5695f7d3c790bf53b27b98a6cc6acba
christianwaldmann <waldmann-christian@web.de>
2021-01-11 02:11:46 +0100

    Refactor input class to be more straightforward

--- Orange/src/Platform/Windows/WindowsInput.cpp
+++ Orange/src/Platform/Windows/WindowsInput.cpp
@@ -1,5 +1,5 @@
 #include "ogpch.h"
-#include "WindowsInput.h"
+#include "Orange/Core/Input.h"
 
 #include "Orange/Core/Application.h"
 #include <GLFW/glfw3.h>
@@ -7,24 +7,21 @@
 
 namespace Orange {
 
-	Input* Input::s_Instance = new WindowsInput();
-
-
-	bool WindowsInput::IsKeyPressedImpl(int keycode) {
+	bool Input::IsKeyPressed(int keycode) {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, keycode);
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
 
-	bool WindowsInput::IsMouseButtonPressedImpl(int button) {
+	bool Input::IsMouseButtonPressed(int button) {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetMouseButton(window, button);
 		return state == GLFW_PRESS;
 	}
 
 
-	std::pair<float, float> WindowsInput::GetMousePositionImpl() {
+	std::pair<float, float> Input::GetMousePosition() {
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xpos, ypos;
 		glfwGetCursorPos(window, &xpos, &ypos);
@@ -33,14 +30,14 @@ namespace Orange {
 	}
 
 
-	float WindowsInput::GetMouseXImpl() {
-		auto [x, y] = GetMousePositionImpl();
+	float Input::GetMouseX() {
+		auto [x, y] = GetMousePosition();
 		return x;
 	}
 
 
-	float WindowsInput::GetMouseYImpl() {
-		auto [x, y] = GetMousePositionImpl();
+	float Input::GetMouseY() {
+		auto [x, y] = GetMousePosition();
 		return y;
 	}
 
